a91e24facd0b17490acd694b4b562c4cd9575f3a
Eghizio <eghizio@gmail.com>
2021-12-20 05:59:12 +0100

    Added meme

--- src/routes/api.js
+++ src/routes/api.js
@@ -5,8 +5,10 @@ const usersRouter = require("./users");
 const router = Router(); /* /api */
 
 router.get("/hello", async (req, res) => {
-    console.log("Hello there");
-    res.json({ "hello": "world" });
+    return res.json({
+        "hello": "there",
+        "url": "http://localhost:1337/meme.png",
+    });
 });
 
 router.use("/users", usersRouter);
